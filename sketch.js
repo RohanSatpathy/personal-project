f8e6c3afbe69ee634542fbd537d378805481817b
RohanSatpathy <88677815+RohanSatpathy@users.noreply.github.com>
2022-09-30 20:54:24 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var ball, paddle, rect1, rect2, bg, oppPaddle1, oppPaddle2;
+
+function preload(){
+  bg = loadImage("/bg.png");
+  ball = loadImage("/bball.png");
+}
+
+function setup(){
+  var paddle = createSprite();
+}
+
+function draw(){
+  createCanvas(800, 400);
+}
